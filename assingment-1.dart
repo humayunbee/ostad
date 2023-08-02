ecd900233af95e43c094d5ea673a5d7ca0ed68bd
Md. Humayun Farid <humayunbee@gmail.com>
2023-08-02 20:26:54 +0600

    Add files via upload

---
+++ assingment-1.dart
@@ -0,0 +1,29 @@
+
+// Assingment 1 Submited By Md. Humayun Farid
+
+void main() {
+  const int a = 7;
+  const int b = 3;
+
+  print("a = $a, b = $b");
+  
+  // Addition
+  int add = a + b;
+  print("Addition: $a + $b = $add");
+  
+  // Subtraction
+  int sub = a - b;
+  print("Subtraction: $a - $b = $sub");
+  
+  // Multiplication
+  int mult = a * b;
+  print("Multiplication: $a * $b = $mult");
+  
+  // Division
+  double div = a / b;
+  print("Division: $a / $b = $div");
+  
+  // Modulus
+  int mod = a % b;
+  print("Modulus: $a % $b = $mod");
+}
